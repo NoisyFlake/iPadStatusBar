0824584c284042d63b681910d787a7145cd88e85
NoisyFlake <joelpoick@me.com>
2019-03-20 17:41:36 +0100

    Add new preference banner

--- preferences/ISSListController.h
+++ preferences/ISSListController.h
@@ -2,5 +2,11 @@
 #import <Preferences/PSSpecifier.h>
 
 @interface ISSListController : PSListController
+@end
 
+@interface iPadStatusBarLogo : PSTableCell {
+	UILabel *background;
+	UILabel *tweakName;
+	UILabel *version;
+}
 @end
